82cf3bf4b0cf46d16fa8591bc2173d5f8e15c20a
junlbj <junlbj@cn.ibm.com>
2016-04-28 11:28:07 +0800

    223601 - Update build pipeline to build the Chrome extension used to register widgets

---
+++ src/Widget Registry/contentscript.js
@@ -0,0 +1,18 @@
+
+var widget = chrome.extension.getURL('widget.json');
+var xhr = new XMLHttpRequest();
+xhr.overrideMimeType("application/json");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState == 4 && xhr.status == "200") {
+    localStorage.setItem('widget-json', xhr.responseText);
+    var pageScript = document.createElement('script');
+    pageScript.src = chrome.extension.getURL('page.js');
+    pageScript.onload = function() {
+      this.parentNode.removeChild(this); 
+    };
+    (document.head||document.documentElement).appendChild(pageScript);
+  }
+};
+xhr.open('GET', widget, true);
+xhr.send();
+
