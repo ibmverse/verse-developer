5256bf6fda3b2a160dc52215ed99d3d1c16c1870
Panpan Lin <panpanli@ie.ibm.com>
2016-09-08 10:15:07 +0100

    248438: rename widget to application

--- src/contentscript.js
+++ src/contentscript.js
@@ -3,20 +3,20 @@
  * Licensed under the MIT License.
  */
 
-var widget = chrome.extension.getURL('widget.json');
+var apps = chrome.extension.getURL('applications.json');
 var xhr = new XMLHttpRequest();
 xhr.overrideMimeType("application/json");
 xhr.onreadystatechange = function () {
   if (xhr.readyState == 4 && xhr.status == "200") {
-    localStorage.setItem('widget-json', xhr.responseText);
+    localStorage.setItem('app-json', xhr.responseText);
     var pageScript = document.createElement('script');
     pageScript.src = chrome.extension.getURL('page.js');
     pageScript.onload = function() {
-      this.parentNode.removeChild(this); 
+      this.parentNode.removeChild(this);
     };
     (document.head||document.documentElement).appendChild(pageScript);
   }
 };
-xhr.open('GET', widget, true);
+xhr.open('GET', apps, true);
 xhr.send();
 
